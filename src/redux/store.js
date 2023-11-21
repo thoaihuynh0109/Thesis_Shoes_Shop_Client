9e035506e1cf914fefc251c667d9a2ddf60a3403
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-11-21 21:41:26 +0700

    update more Reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,15 @@
 import { legacy_createStore as createStore, combineReducers } from 'redux';
 import { cartReducer } from './CartManagement/cartReducer';
 import filterReducer from './FilterManagement/filterReducer';
-
+import wishlistReducer from './WishListManagement/wishlistReducer';
+import productDetailsReducer from './ProductDetails/productDetailsReducer';
+import searchReducer from './SearchManagemenet/searchReducer';
 const rootReducer = combineReducers({
     cart: cartReducer,
     filter: filterReducer,
+    wishlist: wishlistReducer,
+    productDetail: productDetailsReducer,
+    search: searchReducer,
 });
 
 const store = createStore(rootReducer);
