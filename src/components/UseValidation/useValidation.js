138c49184f14143c455bb37f4b3772f161f0d141
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-11-23 23:44:16 +0700

    validate data when user input

---
+++ src/components/UseValidation/useValidation.js
@@ -0,0 +1,165 @@
+import { useState } from 'react';
+
+const useValidation = (initialState) => {
+    const [state, setState] = useState({
+        value: initialState.value || '',
+        message: '',
+        isShow: initialState.isShow || false,
+    });
+
+    const validateRequired = () => {
+        if (state.value.trim() === '') {
+            setState({
+                ...state,
+                message: 'This field is required.',
+            });
+            return false;
+        }
+        setState({
+            ...state,
+            message: '',
+        });
+        return true;
+    };
+
+    const validateEmail = () => {
+        if (state.value.trim() === '') {
+            setState({
+                ...state,
+                message: 'Please enter an email address.',
+            });
+            return false;
+        } else {
+            let validEmail = state.value.toLowerCase().match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+            if (validEmail) {
+                // Additional check for Gmail
+                if (state.value.toLowerCase().endsWith('@gmail.com')) {
+                    setState({
+                        ...state,
+                        message: '',
+                    });
+                    return true;
+                }
+                // else {
+                //     setState({
+                //         ...state,
+                //         message: 'Only Gmail addresses are allowed.',
+                //     });
+                //     return false;
+                // }
+            } else {
+                setState({
+                    ...state,
+                    message: 'Invalid email address.',
+                });
+                return false;
+            }
+        }
+    };
+
+    const validatePassword = () => {
+        if (state.value === '') {
+            setState({
+                ...state,
+                message: 'Please enter a password.',
+            });
+            return false;
+        } else if (state.value.length < 6) {
+            setState({
+                ...state,
+                message: 'Password must be at least 6 characters long.',
+            });
+            return false;
+        }
+        setState({
+            ...state,
+            message: '',
+        });
+        return true;
+    };
+
+    const validateConfirmPassword = (password) => {
+        if (state.value === '') {
+            setState({
+                ...state,
+                message: 'Please confirm your password.',
+            });
+            return false;
+        } else if (state.value !== password) {
+            setState({
+                ...state,
+                message: 'Passwords do not match.',
+            });
+            return false;
+        }
+        setState({
+            ...state,
+            message: '',
+        });
+        return true;
+    };
+
+    const validatePhone = () => {
+        if (state.value === '') {
+            setState({
+                ...state,
+                message: '',
+            });
+        } else {
+            let validPhone = state.value.match(/(0[3|5|7|8|9])+([0-9]{8})\b/g);
+            if (validPhone) {
+                setState({
+                    ...state,
+                    message: '',
+                });
+            } else {
+                setState({
+                    ...state,
+                    message: 'Invalid phone number.',
+                });
+            }
+        }
+    };
+
+    const validateRequiredWithoutDigits = () => {
+        if (/\d/.test(state.value)) {
+            setState({
+                ...state,
+                message: 'Không Được Tồn Tại Số Trong Tên!',
+            });
+            return false;
+        } else if (state.value.trim() === '') {
+            setState({
+                ...state,
+                message: 'This field is required.',
+            });
+            return false;
+        }
+        setState({
+            ...state,
+            message: '',
+        });
+        return true;
+    };
+
+    const handleShow = () => {
+        setState({
+            ...state,
+            isShow: !state.isShow,
+        });
+    };
+
+    return {
+        state,
+        setState,
+        validateRequired,
+        validateEmail,
+        validatePassword,
+        validateConfirmPassword,
+        validatePhone,
+        validateRequiredWithoutDigits,
+        handleShow,
+    };
+};
+
+export default useValidation;
