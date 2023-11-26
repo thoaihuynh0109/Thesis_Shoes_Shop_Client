27d89f233ab520d3bdbca1590c6e7c3901e1fccf
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-11-26 17:44:53 +0700

    call components

--- src/pages/Checkout/ProductsInCard/AddressStep/index.js
+++ src/pages/Checkout/ProductsInCard/AddressStep/index.js
@@ -1,23 +1,12 @@
-import {
-    Box,
-    Container,
-    Typography,
-    Stack,
-    Checkbox,
-    FormControlLabel,
-    Paper,
-    Button,
-} from '@mui/material';
+import { Box, Typography, Stack, Button } from '@mui/material';
 
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import { useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './AddressStep.module.scss';
 import classNames from 'classnames/bind';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import { DialerSip } from '@mui/icons-material';
 import ChooseAddress from './ChooseAddress';
+import ShowDeliveryInformation from './ShowDeliveryInformation';
 const cx = classNames.bind(styles);
 
 const CustomizeTypography = styled(Typography)(({ fs }) => ({
@@ -30,99 +19,60 @@ const CustomizeButton = styled(Button)(({ fs }) => ({
 }));
 
 function AddressStep() {
-    const [age, setAge] = useState('');
+    const navigate = useNavigate();
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
+    const [deliveryAddress, setDeliveryAddress] = useState({
+        fullName: '',
+        phoneNumber: '',
+        address: '',
+        // updatedFullName: '',
+        // updatedPhoneNumber: '',
+        // updatedAddress: '',
+    });
+    // Callback function để cập nhật địa chỉ khi người dùng chọn
+    const handleAddressChange = (selectedAddress) => {
+        // Make sure selectedAddress is defined before updating the state
+        if (selectedAddress) {
+            setDeliveryAddress({
+                updatedFullName: selectedAddress.fullName || '',
+                updatedPhoneNumber: selectedAddress.phoneNumber || '',
+                updatedAddress: selectedAddress.address || '',
+            });
+        }
     };
 
     return (
         <Box sx={{ height: '100%' }}>
-            <Stack direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 2, md: 4 }}>
-                {/* delivery address*/}
-                <Box>
-                    <CustomizeTypography variant="body1">
-                        Choose a delivery address:
-                    </CustomizeTypography>
-                    {/* Choose address */}
-                    <ChooseAddress />
-
-                    <FormControlLabel
-                        // increase font size for checkbox
-                        control={<Checkbox sx={{ '& .MuiSvgIcon-root': { fontSize: '22px' } }} />}
-                        label={
-                            <CustomizeTypography sx={{ fontSize: '15px' }}>
-                                Use the delivery address as the billing address.
-                            </CustomizeTypography>
-                        }
-                    />
-                </Box>
-
-                {/* billing address */}
-                <Box>
-                    <Box sx={{ ml: 29 }}>
-                        <CustomizeTypography variant="body1">
-                            Choose a billing address:
-                        </CustomizeTypography>
-
-                        <ChooseAddress />
-                    </Box>
-                </Box>
-            </Stack>
+            {/* <ChooseAddress onAddressChange={handleAddressChange} /> */}
 
             <Stack
                 direction={{ xs: 'column', sm: 'row' }}
                 spacing={{ xs: 1, sm: 2, md: 4 }}
-                sx={{ display: 'flex', justifyContent: 'space-between', mt: 4 }}
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    mt: 4,
+                    alignItems: 'center',
+                }}
             >
                 <Box>
-                    <ShowDeliveryInformation />
-                </Box>
-                <Box>
-                    <ShowDeliveryInformation />
+                    {/* Pass the onDelete callback to the ShowDeliveryInformation component */}
+                    <ShowDeliveryInformation
+                        deliveryAddress={deliveryAddress}
+                        // onDelete={handleDeleteAddress}
+                    />
                 </Box>
             </Stack>
-            <CustomizeButton sx={{ mt: 2, fontSize: '14px' }} variant="contained">
+            {/* <CustomizeButton
+                sx={{ mt: 2, fontSize: '14px' }}
+                variant="contained"
+                onClick={() => {
+                    navigate('/add-new-address');
+                }}
+            >
                 Add A New Address
-            </CustomizeButton>
+            </CustomizeButton> */}
         </Box>
     );
 }
 export default AddressStep;
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
-
-function ShowDeliveryInformation() {
-    return (
-        <Box sx={{ border: '1px solid #ebebeb', width: '500px', p: 2 }}>
-            <CustomizeTypography variant="h4" gutterBottom>
-                YOUR DELIVERY ADDRESS
-            </CustomizeTypography>
-            <Box sx={{ border: '1px solid #ebebeb' }}></Box>
-            <CustomizeTypography variant="body2" mt={2}>
-                Luna Kei
-            </CustomizeTypography>
-            <CustomizeTypography variant="body2">Japan HCMUTE</CustomizeTypography>
-            <CustomizeTypography variant="body2">
-                Đệ Nhị Số 2 Võ Văn Ngân Đệ Tam Số 3 Võ Văn Ngân
-            </CustomizeTypography>
-            <CustomizeTypography variant="body2">
-                HCMUTE Vip Pro Max, Alabama 09982
-            </CustomizeTypography>
-            <CustomizeTypography variant="body2">United States</CustomizeTypography>
-            <CustomizeTypography variant="body2">0123</CustomizeTypography>
-            <CustomizeTypography gutterBottom variant="body2">
-                0123
-            </CustomizeTypography>
-            <CustomizeButton variant="contained" endIcon={<ArrowForwardIosIcon />}>
-                Update
-            </CustomizeButton>
-        </Box>
-    );
-}
