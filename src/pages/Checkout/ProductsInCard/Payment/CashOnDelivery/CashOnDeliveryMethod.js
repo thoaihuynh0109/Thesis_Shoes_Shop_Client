a1ba2952ec975d892c933652d6825a45d60235a1
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-11-28 15:24:29 +0700

    may COD Method

---
+++ src/pages/Checkout/ProductsInCard/Payment/CashOnDelivery/CashOnDeliveryMethod.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Box, Radio, RadioGroup, FormControlLabel, Button, Typography } from '@mui/material';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
+import SummaryStep from '../../SummaryStep';
+
+// Separate UI components for each payment method
+
+function CashOnDeliveryMethod() {
+    return (
+        <Box sx={{ mb: 2, mt: 4, ml: 2 }}>
+            <SummaryStep></SummaryStep>
+        </Box>
+    );
+}
+
+export default CashOnDeliveryMethod;
