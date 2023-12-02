b0968e668fbbbdb795c6f267883c744705b60d62
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-02 09:35:11 +0700

    add product to cart and show message successfully

--- src/pages/Home/TabProducts/BestSeller.js
+++ src/pages/Home/TabProducts/BestSeller.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import RecipeReviewCard from './ProductsCard';
@@ -7,45 +7,12 @@ import RecipeReviewCard from './ProductsCard';
 import { CustomizeButton } from '~/components/CustomizeButton/CustomizeButton';
 import { MakeProductsCard } from '~/components/MakeProductCards/MakeProductCards';
 import { products } from './MakeProductsCardItem';
-
-// const products = [
-//     {
-//         id: 1,
-//         img: 'https://www.bike-discount.de/media/image/6f/89/4b/adidas_Terrex-Free-Hiker-2-Low-GTX-Wanderschuhe_IG5459_2.jpg',
-//         title: 'Jordan',
-//         price: '3,600,000',
-//         rating: 4,
-//         label: false,
-//         labelNew: true,
-//     },
-//     {
-//         id: 2,
-//         img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1698215220/Gimme-shoes-images/Adidas/adidas-rapidmove-trainers_udkzcn.jpg',
-//         title: 'RAPIDMOVE TRAINER',
-//         price: '3,200,000',
-//         rating: 3,
-//         label: false,
-//     },
-//     {
-//         id: 3,
-//         img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1698216174/Gimme-shoes-images/Adidas/if2649_wht_01_vkqpnt.jpg',
-//         title: 'FORUM LOW SHOES',
-//         price: '2,600,000',
-//         rating: 4,
-//         label: false,
-//     },
-//     {
-//         id: 4,
-//         img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1698222786/Gimme-shoes-images/Puma/Suede%20Brand%20Love.jpg',
-//         title: 'Suede Brand Love',
-//         price: '2,350,000',
-//         rating: 4,
-//         label: true,
-//     },
-// ];
+import { ToastMessage2 } from '~/components/MakeProductCards/MakeProductCards';
 
 function BestSeller() {
     const navigate = useNavigate();
+    const [showToast, setShowToast] = useState(false);
+    const [toastMessage, setToastMessage] = useState('');
 
     const handleNavigateToShop = () => {
         navigate('/shop');
@@ -64,9 +31,21 @@ function BestSeller() {
                         label={product.label}
                         labelNew={product.labelNew}
                         sx={{ margin: '0 20px' }}
+                        showToast={showToast}
+                        setShowToast={setShowToast}
+                        // show suitable toast message
+                        toastMessage={toastMessage}
+                        setToastMessage={setToastMessage}
                     />
                 ))}
             </Box>
+            <ToastMessage2
+                // message="Product added to cart!"
+                message={toastMessage}
+                type="success"
+                showToast={showToast}
+                setShowToast={setShowToast}
+            />
             <CustomizeButton variant="contained" onClick={handleNavigateToShop}>
                 More Items
             </CustomizeButton>
