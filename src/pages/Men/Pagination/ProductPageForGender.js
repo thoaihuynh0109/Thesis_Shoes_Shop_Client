5901ddce0aec4e27aa62e7f3884391f6617206e5
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-02 09:40:53 +0700

    sperate complete LoadMore

--- src/pages/Men/Pagination/ProductPageForGender.js
+++ src/pages/Men/Pagination/ProductPageForGender.js
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
-import {
-    Box,
-    IconButton,
-    TextField,
-    Grid,
-    ListItemText,
-    ListItemButton,
-    Checkbox,
-} from '@mui/material';
+import { Box, IconButton, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { AttachMoneyOutlined } from '@mui/icons-material';
 import SearchIcon from '@mui/icons-material/Search';
-import StoreIcon from '@mui/icons-material/Store';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CustomTypography from '~/components/CustomTyporaphy/CustomTyporaphy';
 import EmptyCard from '~/pages/Checkout/EmptyCard/EmptyCard';
 import SortingSection from '~/pages/Shop/Pagination/SortingSection';
 import { LoadMoreProduct } from './RenderProductForGender';
 import useProductFilter from '../Filter/MakeUseProductFilter';
+import FilterBrandPriceByAccordion from '../FilterProductForGender';
 
 export default function ProductPageForGender({ selectedGender }) {
     const {
@@ -50,7 +37,8 @@ export default function ProductPageForGender({ selectedGender }) {
     const navigate = useNavigate();
 
     return (
-        <Box sx={{ flexGrow: 1, minHeight: '500vh', mt: 4 }}>
+        <Box sx={{ flexGrow: 1, minHeight: '100vh', mt: 4 }}>
+            {/* <Box sx={{ flexGrow: 1, minHeight: '500vh', mt: 4 }}> */}
             <Box>
                 <CustomTypography sx={{ fontWeight: 'bold', fontSize: '18px', mt: 4, mb: 2 }}>
                     Lọc Sản Phẩm
@@ -91,7 +79,7 @@ export default function ProductPageForGender({ selectedGender }) {
             </Box>
             <Box>
                 {hasProducts ? (
-                    <>
+                    <Box sx={{ ml: 14, mr: 12 }}>
                         <LoadMoreProduct
                             getValue={getValue}
                             _DATA={_DATA}
@@ -104,113 +92,11 @@ export default function ProductPageForGender({ selectedGender }) {
                             // selectedGender={'Male'}
                             selectedGender={selectedGender}
                         />
-                    </>
+                    </Box>
                 ) : (
                     <EmptyCard message={'Không có sản phẩm phù hợp'} />
                 )}
             </Box>
         </Box>
-    );
-}
-
-// filter product for men and women page
-function FilterBrandPriceByAccordion({
-    handleBrandFilter,
-    selectedBrands,
-    handlePriceFilter,
-    selectedPriceRange,
-}) {
-    const brands = ['Nike', 'Adidas', 'Puma', 'New Balance', 'Nai Kì'];
-    const prices = [
-        'Dưới 1,000,000đ',
-        '1,000,000đ - 2,000,000đ',
-        '2,000,000đ - 3,000,000đ',
-        'Trên 3,000,000đ',
-    ];
-    return (
-        <div>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <StoreIcon fontSize="large" />
-                        <CustomTypography sx={{ ml: 4, fontSize: '16px', fontWeight: 'bold' }}>
-                            Thương Hiệu
-                        </CustomTypography>
-                    </Box>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {brands.map((brand, index) => (
-                        <ListItemButton
-                            key={index}
-                            selected={selectedBrands.includes(brand)}
-                            onClick={() => handleBrandFilter(brand)}
-                        >
-                            <Checkbox
-                                checked={selectedBrands.includes(brand)}
-                                onChange={() => handleBrandFilter(brand)}
-                            />
-                            <ListItemText
-                                primary={
-                                    <CustomTypography sx={{ fontSize: '14px' }} variant="body1">
-                                        {brand}
-                                    </CustomTypography>
-                                }
-                            />
-                        </ListItemButton>
-                    ))}
-                </AccordionDetails>
-            </Accordion>
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel2a-content"
-                    id="panel2a-header"
-                >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'space-between',
-                        }}
-                    >
-                        <AttachMoneyOutlined fontSize="large" />
-                        <CustomTypography sx={{ ml: 4, fontSize: '16px', fontWeight: 'bold' }}>
-                            Giá
-                        </CustomTypography>
-                    </Box>
-                </AccordionSummary>
-                <AccordionDetails>
-                    {prices.map((price, index) => (
-                        <ListItemButton
-                            key={index}
-                            selected={selectedPriceRange.includes(price)}
-                            onClick={() => handlePriceFilter(price)}
-                        >
-                            <Checkbox
-                                checked={selectedPriceRange.includes(price)}
-                                onChange={() => handlePriceFilter(price)}
-                            />
-                            <ListItemText
-                                primary={
-                                    <CustomTypography sx={{ fontSize: '14px' }} variant="body1">
-                                        {price}
-                                    </CustomTypography>
-                                }
-                            />
-                        </ListItemButton>
-                    ))}
-                </AccordionDetails>
-            </Accordion>
-        </div>
     );
 }
