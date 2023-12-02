88653fd2bb5fdae95893bc39c25487ad14eedeeb
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-02 09:39:42 +0700

    change styles for men

--- src/pages/Men/Men.js
+++ src/pages/Men/Men.js
@@ -5,8 +5,7 @@ import MakeUIForMenAndWomenPage from './Pagination/MakeUIForMenAndWomenPage';
 
 function Men() {
     return (
-        // <Box sx={{ minHeight: '50vh', mt: 4, ml: 5, mr: 4 }}>
-        <Box sx={{ minHeight: '50vh', mt: 2 }}>
+        <Box sx={{ minHeight: '80vh', mt: 2, mb: 4 }}>
             <MakeUIForMenAndWomenPage forWomen={false} />
             <ProductPageForGender selectedGender={'Male'} />
         </Box>
