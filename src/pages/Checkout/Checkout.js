b239d3837e8405027549310b17e65c524dd012b8
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-03 19:13:01 +0700

    re-make checkout pages

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Container, Box } from '@mui/material';
 import EmptyCard from './EmptyCard/EmptyCard';
-import ProductsInCard from '~/pages/Checkout/ProductsInCard/ProductsInCard';
+// import ProductsInCard from '~/pages/Checkout/ProductsInCard/ProductsInCard';
+import ProductsInCard from '~/pages/Checkout/ProductsInCard/ProductsInCard2';
 
 function Checkout() {
     const [checked, setChecked] = useState(true);
