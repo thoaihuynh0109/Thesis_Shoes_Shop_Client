94c85b3a08c9de609c52b43acdb6678210cb8e7f
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-09 13:25:13 +0700

    update 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { privateRoutes, publicRoutes } from './routes';
 import DefaultLayout from './layouts/DefaultLayout/DefaultLayout';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import PageNotFound from './pages/NotFound/PageNotFound';
 
 function App() {
     return (
         <PayPalScriptProvider options={{ 'client-id': process.env.REACT_APP_PAYPAL_CLIENT_ID }}>
             <Router>
-                <div>
-                    <Routes>
-                        {/* for guest and authenticated */}
-                        {publicRoutes.map((route, index) => {
-                            const Page = route.component;
-                            let Layout = route.layout || DefaultLayout;
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Page />
-                                        </Layout>
-                                    }
-                                ></Route>
-                            );
-                        })}
+                <Routes>
+                    {/* for guest --> not to login into system*/}
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        const Layout = route.layout || DefaultLayout;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
 
-                        {/* for admin */}
-                        {privateRoutes.map((route, index) => {
-                            const Page = route.component;
-                            let Layout = route.layout || DefaultLayout;
-                            return (
-                                <Route
-                                    key={index}
-                                    path={route.path}
-                                    element={
-                                        <Layout>
-                                            <Page />
-                                        </Layout>
-                                    }
-                                ></Route>
-                            );
-                        })}
-                    </Routes>
-                </div>
+                    {/* for admin and authenticated  */}
+                    {privateRoutes.map((route, index) => {
+                        const Page = route.component;
+                        const Layout = route.layout || DefaultLayout;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+
+                    {/* any path is not defined will be changed to /404 */}
+                    <Route path="/*" element={<Navigate to="/404" />} />
+                    <Route
+                        path="/404"
+                        element={
+                            <DefaultLayout>
+                                <PageNotFound />
+                            </DefaultLayout>
+                        }
+                    />
+                </Routes>
             </Router>
         </PayPalScriptProvider>
     );
