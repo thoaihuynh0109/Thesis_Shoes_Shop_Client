9f0d45bfc2d513f6ce8ffb63a6f2810752815547
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-10 21:13:15 +0700

    show 10 products for most viewed

--- src/pages/Home/MostProductsViewed/MostProductsViewed.js
+++ src/pages/Home/MostProductsViewed/MostProductsViewed.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import { ArrowBackIos } from '@mui/icons-material';
 // import GridProducts, { ResponsiveGrid, products} from './GridProducts';
-import { Box, styled, Paper, Grid, IconButton, Button, Typography, Container, Divider} from '@mui/material';
+import {
+    Box,
+    styled,
+    Paper,
+    Grid,
+    IconButton,
+    Button,
+    Typography,
+    Container,
+    Divider,
+} from '@mui/material';
 import ResponsiveViewedProducts from './GridProducts';
 
-
-
 export default function MostProductsViewed() {
-    
-
     return (
         <Box sx={{ mt: 10 }}>
-            <ResponsiveViewedProducts/>
+            <ResponsiveViewedProducts />
         </Box>
     );
 }
-
