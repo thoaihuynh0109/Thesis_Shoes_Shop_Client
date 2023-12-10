0a90931b7f002f4f137b3aaaf41955f17ee4aac9
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-10 15:36:08 +0700

    modified styles for 2 buttons

--- src/pages/Home/SuggestCollection/SuggestCollection.js
+++ src/pages/Home/SuggestCollection/SuggestCollection.js
@@ -7,64 +7,6 @@ import CustomTypography from '~/components/CustomTyporaphy/CustomTyporaphy';
 import { MakeProductsCard } from '~/components/MakeProductCards/MakeProductCards';
 import { ToastMessage2 } from '~/components/MakeProductCards/MakeProductCards';
 import productService from '~/services/productServices';
-const products = [
-    {
-        id: 1,
-        img: 'https://www.bike-discount.de/media/image/6f/89/4b/adidas_Terrex-Free-Hiker-2-Low-GTX-Wanderschuhe_IG5459_2.jpg',
-        title: 'Jordan',
-        price: '3,600,000',
-        rating: 4,
-        label: false,
-    },
-    {
-        id: 2,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700364891/Gimme-shoes-images/Adidas/ultrabounce-shoes-core-black-hp5786-0_ev1uxk_hiytlr.png',
-        title: 'RAPIDMOVE TRAINER',
-        price: '3,200,000',
-        rating: 3,
-        label: false,
-    },
-    {
-        id: 3,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700364891/Gimme-shoes-images/Adidas/tyle_running_trang_hp2757_01_standard_d50cb46326c3436682610a2d02b21c1d_464ae878e179491c97840eebfda414dc_grande_nanuat_tcwspk.png',
-        title: 'FORUM LOW SHOES',
-        price: '2,600,000',
-        rating: 4,
-        label: false,
-    },
-    {
-        id: 4,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700375101/Gimme-shoes-images/Puma/Suede_Brand_Love_k1wihf.png',
-        title: 'Suede Brand Love',
-        price: '2,350,000',
-        rating: 4,
-        label: true,
-    },
-    {
-        id: 5,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700375100/Gimme-shoes-images/Puma/SEASONS_Voyage_NITRO_3_dhfjcw.png',
-        title: 'SEASONS Voyage NITRO™ 3',
-        price: '2,400,000',
-        rating: 4,
-        label: true,
-    },
-    {
-        id: 6,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700364892/Gimme-shoes-images/Adidas/4-9_jutp8f_zqsikt.png',
-        title: 'TRAINER V SHOES',
-        price: '2,200,000',
-        rating: 4,
-        label: false,
-    },
-    {
-        id: 7,
-        img: 'https://res.cloudinary.com/dd4gcajeh/image/upload/v1700374884/Gimme-shoes-images/New%20Balance/FuelCell_2190.jgp_wksaga_djc1i7.png',
-        title: 'FuelCell 2190',
-        price: '3,289,765',
-        rating: 5,
-        label: false,
-    },
-];
 
 function SuggestCollection() {
     return (
@@ -113,8 +55,18 @@ function CustomSuggestCollection() {
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Button
-                sx={{ mr: '-12px', zIndex: 1, height: '40px', mt: '-5%', color: '#9d3030' }}
+                sx={{
+                    mr: '-12px',
+                    zIndex: 1,
+                    height: '40px',
+                    mt: '-5%',
+                    // color: '#9d3030',
+                    '&:hover': { backgroundColor: 'transparent' },
+                    '&:focus': { outline: 'none' }, // Remove outline on focus
+                }}
                 onClick={handleGoToPrevImage}
+                // Disable touch ripple effect
+                disableTouchRipple
             >
                 <ArrowBackIos sx={{ fontSize: '36px' }} />
             </Button>
@@ -155,8 +107,18 @@ function CustomSuggestCollection() {
                 </Box>
             </Container>
             <Button
-                sx={{ ml: '10px', height: '40px', mt: '-5%', color: '#9d3030' }}
+                sx={{
+                    ml: '10px',
+                    height: '40px',
+                    mt: '-5%',
+                    // color: '#9d3030',
+                    // Remove outline on focus
+                    '&:focus': { outline: 'none' },
+                    '&:hover': { backgroundColor: 'transparent' },
+                }}
                 onClick={handleGoToNextImage}
+                // Disable touch ripple effect
+                disableTouchRipple
             >
                 <ArrowForwardIosIcon sx={{ fontSize: '36px', ml: '12px' }} />
             </Button>
